6d5f5f30ef459d485d661620dfceafa188caef06
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-03-22 13:39:59 -0300

    Create ex012.js

---
+++ aulas/aula12/ex012.js
@@ -0,0 +1,13 @@
+var agora = new Date
+var hora = agora.getHours()
+console.log(`Agora são ${hora} horas.`)
+
+if (hora <= 12) {
+    console.log('Bom dia')
+} else if (hora > 12 && hora < 18) {
+    console.log('Boa tarde')
+} else if (hora < 24){
+    console.log('Boa noite')
+} else {
+    console.log('O dia só tem 24 horas')
+}
