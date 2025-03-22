c8399677dfc57dd321cbe2f2bde3bb8d7db605e1
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-03-22 12:04:53 -0300

    Summary

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70.5
+console.log(`A velocidade do seu carro ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log('Você foi multado!')
+}
+
+console.log('Use o cinto de segurança')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Festa em Ipanema meu amor...')
+} else {
+    console.log('GRINGOOOO!')
+}
