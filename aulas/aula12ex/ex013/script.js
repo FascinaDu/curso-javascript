97ffb13c34fc12232936b1e4792e3d86654450c1
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-03-24 11:33:57 -0300

    Summary

--- aulas/aula12ex/ex013/script.js
+++ aulas/aula12ex/ex013/script.js
@@ -3,21 +3,25 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 21 //(apenas para teste)
+    var minuto = data.getMinutes()
+    var hora = 12 //(apenas para teste)
 
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora}:${minuto}`
     if (hora >= 0 && hora < 12) {
         //bom dia
         img.src = 'fotomanha.png'
         document.body.style.background = '#87ceeb'
+        document.body.style.color= '#87ceeb'
     } else if (hora >= 12 && hora < 18) {
         //boa tarde
         img.src = 'fototarde.png'
         document.body.style.background = '#b9846f'
+        document.body.style.color= '#b9846f'
     } else {
         //boa noite
         img.src = 'fotonoite.png'
         document.body.style.background = '#403456'
+        document.body.style.color= '#403456'
     }
     
 }
