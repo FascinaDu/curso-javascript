48d444e4c20887da19472485ec17d76d18770ac5
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-03-24 11:57:29 -0300

    Update script.js

--- aulas/aula12ex/ex013/script.js
+++ aulas/aula12ex/ex013/script.js
@@ -8,16 +8,19 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora}:${minuto}`
     if (hora >= 0 && hora < 12) {
         //bom dia
+        msg.innerHTML = 'Bom dia!'
         img.src = 'fotomanha.png'
         document.body.style.background = '#87ceeb'
         document.body.style.color= '#87ceeb'
     } else if (hora >= 12 && hora < 18) {
         //boa tarde
+        msg.innerHTML = 'Boa tarde!'
         img.src = 'fototarde.png'
         document.body.style.background = '#b9846f'
         document.body.style.color= '#b9846f'
     } else {
         //boa noite
+        msg.innerHTML = 'Boa noite!'
         img.src = 'fotonoite.png'
         document.body.style.background = '#403456'
         document.body.style.color= '#403456'
