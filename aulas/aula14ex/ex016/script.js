2520126b0e26caabf643dadb36a8475fde61414c
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-03-25 15:51:51 -0300

    Summary

---
+++ aulas/aula14ex/ex016/script.js
@@ -0,0 +1,12 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.querySelector('div#res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO!] Verifique se marcou todas as caixas!')
+    } else {
+        window.alert('OK')
+    }
+}
