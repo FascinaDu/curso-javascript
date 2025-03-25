3480af6db754291c4d894f5fd0c2b5d6387fcacd
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-03-25 17:01:21 -0300

    Summary

--- aulas/aula14ex/ex017/script.js
+++ aulas/aula14ex/ex017/script.js
@@ -1,16 +1,18 @@
 function calcular() {
-    let numero = document.getElementById('txtn')
-    let tabuada = document.getElementById('selalt')
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('selalt')
 
-    if (numero.value.length == 0) {
+    if (num.value.length == 0) {
         window.alert('[ERRO!] Selecione um número')
     } else {
-        let n = Number(numero.value)
+        let n = Number(num.value)
         let t = 1
+        tab.innerHTML = ''
         while (t <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${t} = ${n*t}`
-            tabuada.appendChild(item) 
+            item.value = `tab${t}`
+            tab.appendChild(item) 
             t++
         }
     }
