b509ee6ee9c861975887c687975956ae964c5cc4
FascinaDu <eduardofascinamiranda07@gmail.com>
2025-03-26 11:12:52 -0300

    Summary

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -1,3 +1,12 @@
 let num = [5, 8, 9, 3, 2]
-
-console.log(num)
+num.sort()
+num.push(1)
+console.log(`${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1 ) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
---
+++ aulas/aula15/vetor.js
@@ -0,0 +1,7 @@
+var num = [6, 7, 4, 5, 6, 5, 6, 1, 0]
+num.sort()
+for (let pos in num) {
+    console.log(num[pos])
+}
+
+num.indexOf(9)
